c9618dad39130795a172d2fae9a4fa525f443af5
cmhoftyzer253 <cmhoftyzer@gmail.com>
2025-07-18 13:58:18 -0400

    add multiply and divide unit skeletons

---
+++ rtl/execute/divide.sv
@@ -0,0 +1,14 @@
+import cpu_consts::*;
+
+module divide (
+    input logic [63:0] opr_a_i,
+    input logic [63:0] opr_b_i,
+
+    input logic [2:0] div_func_i,
+
+    output logic [63:0] div_res_o
+);
+
+    output logic [63:0] div_res_o;
+
+endmodule
---
+++ rtl/execute/multiply.sv
@@ -0,0 +1,14 @@
+import cpu_consts::*;
+
+module multiply (
+    input logic [63:0] opr_a_i,
+    input logic [63:0] opr_b_i,
+
+    input logic [2:0] mult_func_i,
+
+    output logic [63:0] mult_res_o
+);
+
+    logic [63:0] mult_res_o;
+
+endmodule

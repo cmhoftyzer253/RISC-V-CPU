3947c83b781f60cf9cfbcdf3bb7b93b4981b6214
cmhoftyzer253 <137729797+cmhoftyzer253@users.noreply.github.com>
2025-07-22 23:27:14 -0400

    update multiply

--- rtl/execute/multiply.sv
+++ rtl/execute/multiply.sv
@@ -10,6 +10,7 @@ module multiply (
     input logic         mult_instr_i;
     input logic [2:0]   mult_func_i,
     input logic [4:0]   rd_addr_i,
+    input logic         word_op_i,
 
     input logic         stall_i,
     input logic         kill_i,
@@ -47,19 +48,20 @@ module multiply (
     logic [2:0] mult_func_s1;
     logic       mult_instr_s1;
     logic [4:0] rd_addr_s1;
+    logic       word_op_s1;
 
-    assign nxt_a_signed     = (mult_func_i == MULH || mult_func_i == MULHSU || mult_func_i == MULW);
-    assign nxt_b_signed     = (mult_func_i == MULH || mult_func_i == MULW);
+    assign nxt_a_signed     = (mult_func_i == MUL || mult_func_i == MULH || mult_func_i == MULHSU);
+    assign nxt_b_signed     = (mult_func_i == MUL || mult_func_i == MULH);
     assign nxt_negate_res   = (opr_a_i[63] && nxt_a_signed) ^ (opr_b_i[63] && nxt_b_signed);
     assign nxt_mulw_negate  = (opr_a_i[31] ^ opr_b_i[31]);
 
     assign nxt_a_high   = get_magnitude(opr_a_i[63:32], nxt_a_signed, opr_a_i[63]);
     assign nxt_b_high   = get_magnitude(opr_b_i[63:32], nxt_b_signed, opr_b_i[63]);
 
-    assign nxt_a_low    = (mult_func_i == MULW) ?   get_magnitude(opr_a_i[31:0], 1'b1, opr_a_i[31]) : 
+    assign nxt_a_low    = (word_op_i) ?   get_magnitude(opr_a_i[31:0], 1'b1, opr_a_i[31]) : 
                                                     get_magnitude(opr_a_i[31:0], nxt_a_signed, opr_a_i[63]);
 
-    assign nxt_b_low    = (mult_func_i == MULW) ?   get_magnitude(opr_b_i[31:0], 1'b1, opr_b_i[31]) : 
+    assign nxt_b_low    = (word_op_i) ?   get_magnitude(opr_b_i[31:0], 1'b1, opr_b_i[31]) : 
                                                     get_magnitude(opr_b_i[31:0], nxt_b_signed, opr_b_i[63]);
 
     always_ff @(posedge clk or posedge reset) begin
@@ -77,6 +79,7 @@ module multiply (
             mult_func_s1    <= 3'b0;
             mult_instr_s1   <= 1'b0;
             rd_addr_s1      <= 5'b0;
+            word_op_s1      <= 1'b0;
         end else begin
             a_signed_s1     <= nxt_a_signed;
             b_signed_s1     <= nxt_b_signed;
@@ -91,6 +94,7 @@ module multiply (
             mult_func_s1    <= mult_func_i;
             mult_instr_s1   <= mult_instr_i;
             rd_addr_s1      <= rd_addr_i;
+            word_op_s1      <= word_op_i;
         end
     end
 
@@ -111,6 +115,7 @@ module multiply (
     logic [2:0] mult_func_s2;
     logic       mult_instr_s2;
     logic [4:0] rd_addr_s2;
+    logic       word_op_s2;
 
     assign nxt_p0 = a_low_s1 * b_low_s1;
     assign nxt_p1 = a_low_s1 * b_high_s1;
@@ -130,6 +135,7 @@ module multiply (
             mult_func_s2    <= 3'b0;
             mult_instr_s2   <= 1'b0;
             rd_addr_s2      <= 5'b0;
+            word_op_s2      <= 1'b0;
         end else begin
             p0_s2 <= nxt_p0;
             p1_s2 <= nxt_p1;
@@ -142,6 +148,7 @@ module multiply (
             mult_func_s2    <= mult_func_s1;
             mult_instr_s2   <= mult_instr_s1;
             rd_addr_s2      <= rd_addr_s1;
+            word_op_s2      <= word_op_s1;
         end
     end
 
@@ -157,11 +164,12 @@ module multiply (
     logic [2:0] mult_func_s3;
     logic       mult_instr_s3;
     logic [4:0] rd_addr_s3;
+    logic       word_op_s3;
 
     assign cross_terms  = p1_s2 + p2_s2;
 
     //if MULW instruction store result in nxt_part sum
-    assign nxt_part_sum     = (mult_func_s2 == MULW) ?  p0_s2 : 
+    assign nxt_part_sum     = (word_op_s2) ?  p0_s2 : 
                                                         {{64{1'b0}}, p0_s2} + ({{63{1'b0}}, cross_terms} << 32);
 
     always_ff @(posedge clk or posedge reset) begin
@@ -174,6 +182,7 @@ module multiply (
             mult_func_s3    <= 3'b0;
             mult_instr_s3   <= 1'b0;
             rd_addr_s3      <= 5'b0;
+            word_op_s3      <= 1'b0;
         end else begin
             part_sum_s3     <= nxt_part_sum;
 
@@ -183,6 +192,7 @@ module multiply (
             mult_func_s3    <= mult_func_s2;
             mult_instr_s3   <= mult_instr_s2;
             rd_addr_s3      <= rd_addr_s2;
+            word_op_s3      <= word_op_s2;
         end
     end
 
@@ -196,10 +206,11 @@ module multiply (
 
     logic [2:0] mult_func_s4;
     logic       mult_instr_s4;
-    logic [4:0] rd_addr_s4;     
+    logic [4:0] rd_addr_s4;
+    logic       word_op_s4;     
 
     //if MULW instruction store in bottom 64 bits of nxt_full_sum - sign extend
-    assign nxt_full_sum = (mult_func_s3 == MULW) ?  {{64{part_sum_s3[63]}}, part_sum_s3} :
+    assign nxt_full_sum = (word_op_s3) ?  {{64{part_sum_s3[63]}}, part_sum_s3} :
                                                     part_sum_s3 + ({{64{1'b0}}, p3_s2} << 64);
 
     always_ff @(posedge clk or posedge reset) begin
@@ -211,6 +222,7 @@ module multiply (
             mult_func_s4    <= 3'b0;
             mult_instr_s4   <= 1'b0;
             rd_addr_s4      <= 5'b0;
+            word_op_s4      <= 1'b0;
         end else begin
             full_sum_s4     <= nxt_full_sum;
 
@@ -220,6 +232,7 @@ module multiply (
             mult_func_s4    <= mult_func_s3;
             mult_instr_s4   <= mult_instr_s3;
             rd_addr_s4      <= rd_addr_s3;
+            word_op_s4      <= word_op_s3;
         end
     end
 
@@ -232,11 +245,11 @@ module multiply (
     
     always_comb begin
         case(mult_func_s4)
-            MUL     : mult_res = final_sum[63:0];
+            MUL     : mult_res = (word_op_s4) ? {{32{final_sum[31]}}, final_sum[31:0]} : 
+                                                final_sum[63:0];
             MULH,
             MULHU,
             MULHSU  : mult_res = final_sum[127:64];
-            MULW    : mult_res = {{32{final_sum[31]}}, final_sum[31:0]};
             default : mult_res = 64'h0;
         endcase
     end

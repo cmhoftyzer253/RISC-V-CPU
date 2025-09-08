20299417e9bf1eeb2cb4d437f2d02e1af8b3086e
cmhoftyzer253 <cmhoftyzer@gmail.com>
2025-09-08 03:46:44 -0400

    make execute top module containg alu, mul, div

--- rtl/execute/execute.sv
+++ rtl/execute/execute.sv
@@ -1,33 +1,92 @@
 import cpu_consts::*;
 
-module execute (
-    input logic [63:0] opr_a_i,
-    input logic [63:0] opr_b_i,
+module execute(
+    input logic         clk,
+    input logic         reset,
+    
+    input logic [63:0]  opr_a_i,
+    input logic [63:0]  opr_b_i,
 
-    input logic [3:0] alu_func_i,
-    input logic       word_op_i,
+    input logic         exu_func_i,
+    input logic         word_op_i,
+    input logic         mul_instr_i,
+    input logic         div_instr_i,
 
-    output logic [63:0] alu_res_o
+    input logic         flush_i,
+    input logic         kill_i,
+
+    output logic [63:0] exu_res_o,
+    output logic        valid_res_o,    
+    output logic        exu_busy_o,
 );
 
-    logic [63:0] alu_res;
+    logic [63:0]    alu_res;
+    logic           alu_res_valid;
 
-    always_comb begin                                                                   
-        case (alu_func_i)
-            OP_ADD : alu_res = opr_a_i + opr_b_i;
-            OP_SUB : alu_res = opr_a_i - opr_b_i;
-            OP_SLL : alu_res = opr_a_i << opr_b_i[5:0];                 
-            OP_SRL : alu_res = opr_a_i >> opr_b_i[5:0];                 
-            OP_SRA : alu_res = $signed(opr_a_i) >>> opr_b_i[5:0];        
-            OP_OR : alu_res = opr_a_i | opr_b_i;
-            OP_AND : alu_res = opr_a_i & opr_b_i;
-            OP_XOR : alu_res = opr_a_i ^ opr_b_i;
-            OP_SLTU : alu_res = {63'h0, opr_a_i < opr_b_i};               
-            OP_SLT : alu_res = {63'h0, $signed(opr_a_i) < $signed(opr_b_i)};    
-            default : alu_res = 64'h0;
-        endcase
-    end
+    logic [63:0]    mul_res;
+    logic           mul_res_valid;
+    logic           mul_busy;
 
-    assign alu_res_o = word_op_i ? {{32{alu_res[31]}}, alu_res[31:0]} : alu_res;
+    logic [63:0]    div_res;
+    logic           div_res_valid;
+    logic           div_busy;
+
+    logic [63:0]    exu_res;
+    logic           valid_res       
+    logic           exu_busy;
+
+    //alu
+    alu u_alu (
+        .opr_a_i        (opr_a_i),
+        .opr_b_i        (opr_b_i),
+        .alu_func_i     (exu_func_i),
+        .word_op_i      (word_op_i),
+        .alu_res_o      (alu_res),
+        .valid_res_o    (alu_res_valid)
+    );
+
+    //multiply unit
+    multiply multiply_u (
+        .clk            (clk),
+        .reset          (reset),
+        .opr_a_i        (opr_a_i),
+        .opr_b_i        (opr_b_i),
+        .mul_instr_i    (mul_instr_i),
+        .mul_func_i     (exu_func_i),
+        .word_op_i      (word_op_i),
+        .flush_i        (flush_i),
+        .kill_i         (kill_i),
+        .mul_res_o      (mul_res),
+        .valid_res_o    (mul_res_valid),
+        .mul_busy_o     (mul_busy)
+    );
+
+    //divide unit
+    divide divide_u (
+        .clk            (clk),
+        .reset          (reset),
+        .opr_a_i        (opr_a_i),
+        .opr_b_i        (opr_b_i),
+        .div_instr_i    (div_instr_i),
+        .div_func_i     (exu_func_i),
+        .word_op_i      (word_op_i),
+        .flush_i        (flush_i),
+        .kill_i         (kill_i),
+        .div_res_o      (div_res),
+        .valid_res_o    (div_res_valid),
+        .div_busy_o     (div_busy)
+    );
+
+    exu_res[63:0]   =   ({64{mul_res_valid}} & div_res[63:0]) |
+                        ({64{div_res_valid}} & mul_res[63:0]) | 
+                        ({64{alu_res_valid}} & alu_res[63:0]);
+
+    exu_busy        =   mul_busy | div_busy;
+    valid_res       =   alu_res_valid | mul_res_valid | div_res_valid;
+
+    //output assignments
+    assign exu_res_o[63:0]      = exu_res[63:0];
+    assign valid_res_o          = valid_res;
+    assign exu_busy_o           = exu_busy;
 
 endmodule

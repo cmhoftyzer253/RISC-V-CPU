aad0993063c2c05fee719c755858a9281b743db1
cmhoftyzer253 <cmhoftyzer@gmail.com>
2025-09-08 03:45:36 -0400

    update syntax & style

--- rtl/execute/alu.sv
+++ rtl/execute/alu.sv
@@ -6,8 +6,10 @@ module alu (
 
     input logic [3:0] alu_func_i,
     input logic       word_op_i,
+    input logic       alu_instr_i,
 
-    output logic [63:0] alu_res_o
+    output logic [63:0] alu_res_o,
+    output logic        valid_res_o
 );
 
     logic [63:0] alu_res;
@@ -28,6 +30,9 @@ module alu (
         endcase
     end
 
-    assign alu_res_o = word_op_i ? {{32{alu_res[31]}}, alu_res[31:0]} : alu_res;
+    assign alu_res_o =  ({64{ word_op_i & alu_instr_i}} & {{32{alu_res[31]}}, alu_res[31:0]}) |
+                        ({64{~word_op_i & alu_instr_i}} & alu_res);
+
+    assign valid_res = alu_instr_i;
 
 endmodule

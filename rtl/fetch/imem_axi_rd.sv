89833eae2bba18652f76113a9745d6c764f96251
cmhoftyzer253 <cmhoftyzer@gmail.com>
2025-10-02 21:34:32 -0400

    update for 32bit instruction output stream

--- rtl/fetch/imem_axi_rd.sv
+++ rtl/fetch/imem_axi_rd.sv
@@ -1,12 +1,16 @@
+import cpu_modules::*;
+import cpu_consts::*;
+
 module imem_axi_rd (
 
     //cpu interface
     input logic             cpu_clk,
     input logic             cpu_reset,
     input logic             ready_i,
-    output logic [511:0]    line_o,
-    output logic            line_valid_o,
-
+    output logic [31:0]     instr_o,
+    output logic            instr_valid_o,
+    output logic            last_instr_o,
+    
     //axi interface
     input logic             axi_clk,
     input logic             axi_resetn,
@@ -20,119 +24,89 @@ module imem_axi_rd (
     output logic [4:0]      exc_code_o
 );
 
-    logic [511:0]   line_axi_q;
-    logic [511:0]   line_cpu_q;
-    logic [511:0]   line_q;
+    logic           fifo_full;
+    logic           fifo_empty;
 
-    logic           error_axi_q;
-    logic           error_cpu_q;
-    logic           error_q;
+    logic           fifo_wr_en;
+    logic           fifo_rd_en;
 
-    logic [2:0]     beat_cnt_q;
-    logic           burst_q;
+    fifo_entry_t    fifo_rd_data;
+    fifo_entry_t    fifo_wr_data;
 
-    logic wptr_axi;
-    logic rptr_cpu;
+    logic           rready;
 
-    logic wptr_cpu1;
-    logic wptr_cpu2;
+    assign fifo_wr_data.error   = (rresp_i != 2'b00);
+    assign fifo_wr_data.last    = rlast_i;
+    assign fifo_wr_data.data    = rdata_i;
 
-    logic rptr_axi1;
-    logic rptr_axi2;
+    assign fifo_wr_en           = rvalid_i & rready;
+    
+    assign rready               = axi_resetn & ~fifo_full;
 
-    logic empty_axi;
-    logic full_cpu;
+    async_fifo_64 u_async_fifo_64 (
+        
+        .axi_clk        (axi_clk),
+        .axi_resetn     (axi_resetn),
+        .wr_en_i        (fifo_wr_en),
+        .wr_data_i      (fifo_wr_data),
+        .wr_full_o      (fifo_full),
 
-    logic rready;
+        .cpu_clk        (cpu_clk),
+        .cpu_reset      (cpu_reset),
+        .rd_en_i        (fifo_rd_en),
+        .rd_data_o      (fifo_rd_data),
+        .rd_valid_o     (instr_valid),
+        .rd_empty_o     (fifo_empty)
+    );
 
-    logic line_valid;
+    logic [63:0]    instr_beat;
 
-    logic first_beat;
-    logic next_beat;
-    logic last_beat;
+    logic           have_beat_q;
 
-    assign rready       = axi_resetn & empty_axi;
-    assign line_valid   = full_cpu;
+    logic           beat_half_sel_q;
 
-    assign empty_axi    = (wptr_axi == rptr_axi2);
-    assign full_cpu     = (wptr_cpu2 != rptr_cpu);
+    logic           instr_last;
+    logic [31:0]    instr;
 
-    assign first_beat   = ~burst_q & empty_axi & rvalid_i & rready;
-    assign next_beat    = burst_q & rvalid_i & rready & ~rlast_i;
-    assign last_beat    = burst_q & rvalid_i & rready & rlast_i;
+    logic exc_valid;
 
-    always_ff @(posedge axi_clk) begin
-        if (~axi_resetn) begin
-            line_axi_q      <= 512'h0;
-            beat_cnt_q      <= 3'b0;
-            burst_q         <= 1'b0;
-            error_axi_q     <= 1'b0;
+    assign fifo_rd_en   =   ~have_beat_q & ~fifo_empty & ~cpu_reset;
 
-            line_q          <= 512'h0;
-            error_q         <= 1'b0;
-            wptr_axi        <= 1'b0;
-        end else begin
-            if (first_beat) begin   
-                burst_q             <= 1'b1;
-                beat_cnt_q          <= 3'b001;
-                error_axi_q         <= (rresp_i != 2'b00);
-                line_axi_q[63:0]    <= rdata_i;
-            end else if (next_beat) begin
-                beat_cnt_q                          <= beat_cnt_q + 3'b001;
-                line_axi_q[64*beat_cnt_q +: 64]     <= rdata_i;
-                if (rresp_i != 2'b00) begin
-                    error_axi_q <= 1'b1;
-                end
-            end
+    assign instr_beat   =   fifo_rd_data.data;
 
-            if (last_beat) begin
-                burst_q     <= 1'b0;
-                line_q      <= {rdata_i, line_axi_q[447:0]};
-                error_q     <= error_axi_q | (rresp_i != 2'b00);
-                wptr_axi    <= ~wptr_axi;
-            end 
-        end
-    end
+    assign exc_valid    =   (beat_half_sel_q & ready_i & instr_valid_o & fifo_rd_data.error);
+    assign instr_last   =   (have_beat_q & fifo_rd_data.last & beat_half_sel_q);
 
-    always_ff @(posedge axi_clk) begin
-        if (~axi_resetn) begin
-            rptr_axi1 <= 1'b0;
-            rptr_axi2 <= 1'b0;
-        end else begin
-            rptr_axi1 <= rptr_cpu;
-            rptr_axi2 <= rptr_axi1;
-        end
-    end
+    assign instr        =   ({32{~beat_half_sel_q}} & instr_beat[31:0]) |
+                            ({32{ beat_half_sel_q}} & instr_beat[63:32]);
 
     always_ff @(posedge cpu_clk) begin
         if (cpu_reset) begin
-            wptr_cpu1       <= 1'b0;
-            wptr_cpu2       <= 1'b0;
-            line_cpu_q      <= 512'h0;
-            error_cpu_q     <= 1'b0;
-            rptr_cpu        <= 1'b0;
+            beat_half_sel_q     <= 1'b0;
+            have_beat_q         <= 1'b0;
         end else begin
-            wptr_cpu1       <= wptr_axi;
-            wptr_cpu2       <= wptr_cpu1;
-
-            if (wptr_cpu2 != rptr_cpu) begin
-                line_cpu_q      <= line_q;
-                error_cpu_q     <= error_q;
-            end
-
-            if (ready_i & full_cpu) begin
-                rptr_cpu <= ~rptr_cpu;
+            if (fifo_rd_en) begin
+                have_beat_q         <= 1'b1;
+                beat_half_sel_q     <= 1'b0;
+            end else if (instr_valid_o & ready_i) begin
+                if (have_beat_q & ~beat_half_sel_q) begin
+                    beat_half_sel_q <= 1'b1;
+                end else if (have_beat_q & beat_half_sel_q) begin
+                    have_beat_q     <= 1'b0;
+                    beat_half_sel_q <= 1'b0;
+                end
             end
         end
     end
 
     //output assignments
-    assign line_o       = line_cpu_q;
-    assign line_valid_o = line_valid;
+    assign instr_o          = instr;
+    assign instr_valid_o    = have_beat_q;
+    assign last_instr_o     = instr_last;
 
-    assign rready_o     = rready;
+    assign rready_o         = rready;
 
-    assign exc_valid_o  = line_valid_o & error_cpu_q;
-    assign exc_code_o   = 5'b00001;
+    assign exc_valid_o      = exc_valid;
+    assign exc_code_o       = 5'b00001;
 
 endmodule

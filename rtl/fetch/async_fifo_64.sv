44dd128fd864131b0367e71da1b46906f59b931f
cmhoftyzer253 <cmhoftyzer@gmail.com>
2025-10-02 21:34:04 -0400

    update fifo

--- rtl/fetch/async_fifo_64.sv
+++ rtl/fetch/async_fifo_64.sv
@@ -2,25 +2,27 @@ import cpu_modules::*;
 import cpu_consts::*;
 
 module async_fifo_64 (
+    //write (axi side) interface
+    input logic         axi_clk,
+    input logic         axi_resetn,
+    input logic         wr_en_i,
+    input fifo_entry_t  wr_data_i,
+    output logic        wr_full_o,
 
     //read (cpu side) interface
     input logic         cpu_clk,
     input logic         cpu_reset,
     input logic         rd_en_i,
-    output logic [63:0] rd_data_o,
+    output fifo_entry_t rd_data_o,
+    output logic        rd_valid_o,
     output logic        rd_empty_o
-
-    //write (axi side) interface
-    input logic         axi_clk,
-    input logic         axi_resetn,
-    input logic         wr_en_i,
-    input logic [63:0]  wr_data_i,
-    output logic        wr_full_o,
 );
 
-    logic [63:0]    mem [7:0];
+    fifo_entry_t    mem [7:0];
 
-    logic [63:0]    rd_data;
+    fifo_entry_t    rd_data_q;
+
+    logic           rd_valid_q;
 
     logic           wr_full;
     logic           rd_empty;
@@ -53,8 +55,8 @@ module async_fifo_64 (
 
     always_ff @(posedge axi_clk) begin
         if (~axi_resetn) begin
-            wr_b_ptr_q  <= 3'b0;
-            wr_gc_ptr_q <= 3'b0;
+            wr_b_ptr_q  <= 4'b0;
+            wr_gc_ptr_q <= 4'b0;
         end else begin
             if (wr_ptr_incr) begin
                 mem[wr_b_ptr_q[2:0]] <= wr_data_i;
@@ -65,6 +67,16 @@ module async_fifo_64 (
         end
     end
 
+    always_ff @(posedge axi_clk) begin
+        if (~axi_resetn) begin
+            rd_gc_axi_ptr_q1 <= 4'b0;
+            rd_gc_axi_ptr_q2 <= 4'b0;
+        end else begin
+            rd_gc_axi_ptr_q1 <= rd_gc_ptr_q;
+            rd_gc_axi_ptr_q2 <= rd_gc_axi_ptr_q1;
+        end
+    end
+
     assign rd_ptr_incr    = rd_en_i & ~rd_empty;
 
     assign nxt_rd_b_ptr   = rd_b_ptr_q + rd_ptr_incr;
@@ -72,33 +84,29 @@ module async_fifo_64 (
 
     always_ff @(posedge cpu_clk) begin
         if (cpu_reset) begin
-            rd_b_ptr_q  <= 3'b0;
-            rd_gc_ptr_q <= 3'b0;
+            rd_b_ptr_q  <= 4'b0;
+            rd_gc_ptr_q <= 4'b0;
+            rd_valid_q  <= 1'b0;
+            rd_data_q   <= '0;
         end else begin
+            rd_valid_q <= rd_ptr_incr;
+
+            if (rd_ptr_incr) begin
+                rd_data_q <= mem[rd_b_ptr_q[2:0]];
+            end
+
             rd_b_ptr_q <= nxt_rd_b_ptr;
             rd_gc_ptr_q <= nxt_rd_gc_ptr;
         end
     end
 
-    assign rd_data = mem[rd_b_ptr_q[2:0]];
-
     always_ff @(posedge cpu_clk) begin
         if (cpu_reset) begin
-            wr_gc_cpu_ptr_q1 <= 3'b0;
-            wr_gc_cpu_ptr_q2 <= 3'b0;
+            wr_gc_cpu_ptr_q1 <= 4'b0;
+            wr_gc_cpu_ptr_q2 <= 4'b0;
         end else begin
             wr_gc_cpu_ptr_q1 <= wr_gc_ptr_q;
             wr_gc_cpu_ptr_q2 <= wr_gc_cpu_ptr_q1;
-        end
-    end
-
-    always_ff @(posedge axi_clk) begin
-        if (~axi_resetn) begin
-            rd_gc_axi_ptr_q1 <= 3'b0;
-            rd_gc_axi_ptr_q2 <= 3'b0;
-        end else begin
-            rd_gc_axi_ptr_q1 <= rd_gc_ptr_q;
-            rd_gc_axi_ptr_q2 <= rd_gc_axi_ptr_q1;
         end
     end
 
@@ -107,7 +115,8 @@ module async_fifo_64 (
 
     //output assignments
     assign wr_full_o    = wr_full;
-    assign rd_data_o    = rd_data;
+    assign rd_data_o    = rd_data_q;
+    assign rd_valid_o   = rd_valid_q;
     assign rd_empty_o   = rd_empty;
 
 endmodule

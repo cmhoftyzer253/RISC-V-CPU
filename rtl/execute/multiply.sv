a6a853f2e27dae785e6151bf59bf5dd5e95a1916
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-02-08 03:22:47 -0500

    update multiply unit

--- rtl/execute/multiply.sv
+++ rtl/execute/multiply.sv
@@ -2,268 +2,264 @@ import cpu_consts::*;
 import cpu_modules::*;
 
 module multiply (
-    input logic clk,
-    input logic reset,
+    input logic         clk,
+    input logic         resetn,
 
-    input logic [63:0] opr_a_i,     //multiplicand
-    input logic [63:0] opr_b_i,     //multiplier
-
-    input logic         mul_instr_i,
+    input logic         mul_valid_i,
+    input logic [63:0]  opr_a_i,
+    input logic [63:0]  opr_b_i,
     input logic [3:0]   mul_func_i,
     input logic         word_op_i,
+    output logic        mul_ready_o,
 
-    input logic         flush_i,
-    input logic         kill_i,
-
+    input logic         mul_ready_i,
     output logic [63:0] mul_res_o,
-    output logic        valid_res_o,
-    output logic        mul_busy_o
+    output logic        mul_res_valid_o,
+
+    input logic         flush_i
 );
 
-    //valid signals
-    logic v_s1;
-    logic v_s2;
-    logic v_s3;
-    logic v_s4;
+    logic [31:0]        a_high_q;
+    logic [31:0]        a_low_q;
+    logic [31:0]        b_high_q;
+    logic [31:0]        b_low_q;
 
-    logic v_s1_in;
-    logic v_s2_in;
-    logic v_s3_in;
-    logic v_s4_in;
+    logic               negate_res;
+    logic [3:0]         mul_func;
+    logic               word_op;
 
-    assign v_s1_in = mul_instr_i & ~kill_i;
-    assign v_s2_in = v_s1 & ~flush_i;
-    assign v_s3_in = v_s2 & ~flush_i;
-    assign v_s4_in = v_s3 & ~flush_i;
+    logic [63:0]        p0_q;
+    logic [63:0]        p1_q;
+    logic [63:0]        p2_q;
+    logic [63:0]        p3_q;
 
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            v_s1 <= 1'b0;
-            v_s2 <= 1'b0;
-            v_s3 <= 1'b0;
-            v_s4 <= 1'b0;
-        end else begin
-            v_s1 <= v_s1_in;
-            v_s2 <= v_s2_in;
-            v_s3 <= v_s3_in;
-            v_s4 <= v_s4_in;
-        end
-    end
+    logic [127:0]       part_sum_q;
 
-    // stage 1 - register inputs & split operands
-    logic a_signed_in;
-    logic b_signed_in;
-    logic negate_res_in;
+    logic               a_signed_in;
+    logic               b_signed_in;
 
-    logic negate_res_s1;
+    logic               a_negate_dw;
+    logic               a_correct_dw;
+    logic               a_negate_w;
+    logic               a_correct_w;
 
-    logic [63:0] a_correct;
-    logic [63:0] b_correct;
+    logic               b_negate_dw;
+    logic               b_correct_dw;
+    logic               b_negate_w;
+    logic               b_correct_w;
 
-    logic [31:0] a_high_in;
-    logic [31:0] a_low_in;
-    logic [31:0] b_high_in;
-    logic [31:0] b_low_in;
+    logic [63:0]        a_mag;
+    logic [63:0]        b_mag;
 
-    logic [31:0] a_high_s1;
-    logic [31:0] a_low_s1;
-    logic [31:0] b_high_s1;
-    logic [31:0] b_low_s1;
+    logic [31:0]        a_high_in;
+    logic [31:0]        a_low_in;
+    logic [31:0]        b_high_in;
+    logic [31:0]        b_low_in;
 
-    logic [3:0] mul_func_s1;
-    logic       word_op_s1;
+    logic               negate_res_in;
 
-    assign a_signed_in      =   (mul_func_i == OP_MUL | mul_func_i == OP_MULH | mul_func_i == OP_MULHSU);    
-    assign b_signed_in      =   (mul_func_i == OP_MUL | mul_func_i == OP_MULH);
-    assign negate_res_in    =   (~word_op_i & ((opr_a_i[63] & a_signed_in) ^ (opr_b_i[63] & b_signed_in))) |
-                                ( word_op_i & (opr_a_i[31] ^ opr_b_i[31]));                                
+    logic [63:0]        p0_in;
+    logic [63:0]        p1_in;
+    logic [63:0]        p2_in;
+    logic [63:0]        p3_in;
 
-
-    assign a_correct[63:0]  =   ({64{a_signed_in & opr_a_i[63] & ~word_op_i}}     & twos_comp_64(opr_a_i[63:0])) |
-                                ({64{(~a_signed_in | ~opr_a_i[63]) & ~word_op_i}} & opr_a_i[63:0]) |
-                                ({64{word_op_i &  opr_a_i[31]}}                   & {32'h0, twos_comp_32(opr_a_i[31:0])}) |
-                                ({64{word_op_i & ~opr_a_i[31]}}                   & {32'h0, opr_a_i[31:0]});
-
-    assign b_correct[63:0]  =   ({64{b_signed_in & opr_b_i[63] & ~word_op_i}}     & twos_comp_64(opr_b_i[63:0])) |
-                                ({64{(~b_signed_in | ~opr_b_i[63]) & ~word_op_i}} & opr_b_i[63:0]) |
-                                ({64{word_op_i &  opr_b_i[31]}}                   & {32'h0, twos_comp_32(opr_b_i[31:0])}) |
-                                ({64{word_op_i & ~opr_b_i[31]}}                   & {32'h0, opr_b_i[31:0]});
-
-    assign a_high_in[31:0]  =   a_correct[63:32];
-    assign b_high_in[31:0]  =   b_correct[63:32];
-
-    assign a_low_in[31:0]   =   a_correct[31:0];
-    assign b_low_in[31:0]   =   b_correct[31:0];
-
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            a_high_s1       <= 32'h0;
-            a_low_s1        <= 32'h0;
-            b_high_s1       <= 32'h0;
-            b_low_s1        <= 32'h0;
-
-            negate_res_s1   <= 1'b0;
-
-            mul_func_s1     <= 4'b0;
-            word_op_s1      <= 1'b0;
-        end else begin
-            a_high_s1       <= a_high_in;
-            b_high_s1       <= b_high_in;
-            a_low_s1        <= a_low_in;
-            b_low_s1        <= b_low_in;
-
-            negate_res_s1   <= negate_res_in;
-
-            mul_func_s1     <= mul_func_i;
-            word_op_s1      <= word_op_i;
-        end
-    end
-
-    // stage 2 - calculate partial products
-    logic [63:0] p0_in;
-    logic [63:0] p1_in;
-    logic [63:0] p2_in;
-    logic [63:0] p3_in;
-
-    logic [63:0] p0_s2;
-    logic [63:0] p1_s2;
-    logic [63:0] p2_s2;
-    logic [63:0] p3_s2;
-
-    logic       negate_res_s2;
-
-    logic [3:0] mul_func_s2;
-    logic       word_op_s2;
-
-    assign p0_in[63:0] = a_low_s1[31:0]  * b_low_s1[31:0];
-    assign p1_in[63:0] = a_low_s1[31:0]  * b_high_s1[31:0];
-    assign p2_in[63:0] = a_high_s1[31:0] * b_low_s1[31:0];
-    assign p3_in[63:0] = a_high_s1[31:0] * b_high_s1[31:0];
-
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            p0_s2 <= 64'h0;
-            p1_s2 <= 64'h0;
-            p2_s2 <= 64'h0;
-            p3_s2 <= 64'h0;
-
-            negate_res_s2   <= 1'b0;
-
-            mul_func_s2     <= 4'b0;
-            word_op_s2      <= 1'b0;
-        end else begin
-            p0_s2 <= p0_in;
-            p1_s2 <= p1_in;
-            p2_s2 <= p2_in;
-            p3_s2 <= p3_in;
-
-            negate_res_s2   <= negate_res_s1;
-
-            mul_func_s2     <= mul_func_s1;
-            word_op_s2      <= word_op_s1;
-        end
-    end
-
-    // stage 3 - combine lower 96 bits (((p1 + p2) << 32) + p0)
-    logic [64:0]    cross_terms;
-    logic [127:0]   part_sum_in;
-
-    logic [127:0]   part_sum_s3;
-    logic [63:0]    p3_s3;
-
-    logic       negate_res_s3;
-
-    logic [3:0] mul_func_s3;
-    logic       word_op_s3;
-
-    assign cross_terms[64:0]        = {1'b0, p1_s2[63:0]} + {1'b0, p2_s2[63:0]};
-
-    //if MULW instruction store result in lower 64 bits of part_sum_in
-    assign  part_sum_in[127:0]      =   ({128{word_op_s2}} & {64'h0, p0_s2[63:0]}) |
-                                        ({128{~word_op_s2}} & ({64'h0, p0_s2[63:0]} + ({63'h0, cross_terms[64:0]} << 32)));
-                                                
-
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            part_sum_s3     <= 128'h0;
-            p3_s3           <= 64'h0;
-
-            negate_res_s3   <= 1'b0;
-
-            mul_func_s3     <= 4'b0;
-            word_op_s3      <= 1'b0;
-        end else begin
-            part_sum_s3     <= part_sum_in;
-            p3_s3           <= p3_s2;
-
-            negate_res_s3   <= negate_res_s2;
-
-            mul_func_s3     <= mul_func_s2;
-            word_op_s3      <= word_op_s2;
-        end
-    end
-
-    // stage 4 - add p3 to lower 96 bits
-    logic [127:0]   full_sum_in;
-    logic [127:0]   full_sum_s4;
+    logic [64:0]        cross_terms;
     
-    logic       negate_res_s4;
+    logic [127:0]       part_sum_in;
 
-    logic [3:0] mul_func_s4;
-    logic       word_op_s4;     
+    logic [63:0]        mul_res_corr;
 
-    //if MULW instruction keep part_sum_s3 result
-    assign full_sum_in[127:0]   =   ({128{word_op_s3}}  & part_sum_s3[127:0]) | 
-                                    ({128{~word_op_s3}} & (part_sum_s3[127:0] + {p3_s3[63:0], 64'h0})); 
+    logic [127:0]       full_sum;
+    logic [127:0]       final_sum;
 
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            full_sum_s4     <= 128'h0;
+    mul_state_t         state;
 
-            negate_res_s4   <= 1'b0;
+    always_ff @(posedge clk or negedge resetn) begin
+        if (~resetn) begin
+            a_high_q                    <= 32'h0;
+            a_low_q                     <= 32'h0;
 
-            mul_func_s4     <= 4'b0;
-            word_op_s4      <= 1'b0;
+            b_high_q                    <= 32'h0;
+            b_low_q                     <= 32'h0;
+
+            negate_res                  <= 1'b0;
+            mul_func                    <= 4'b0;
+            word_op                     <= 1'b0;
+
+            p0_q                        <= 64'h0;
+            p1_q                        <= 64'h0;
+            p2_q                        <= 64'h0;
+            p3_q                        <= 64'h0;
+
+            part_sum_q                  <= 128'h0;
+
+            state                       <= S_IDLE;
         end else begin
-            full_sum_s4     <= full_sum_in;
+            case (state)
+                S_IDLE: begin
+                    if (mul_valid_i & ~flush_i) begin
+                        a_high_q        <= a_high_in;
+                        a_low_q         <= a_low_in;
+                        b_high_q        <= b_high_in;
+                        b_low_q         <= b_low_in;
 
-            negate_res_s4   <= negate_res_s3;
+                        negate_res      <= negate_res_in;
+                        mul_func        <= mul_func_i;
+                        word_op         <= word_op_i;
 
-            mul_func_s4    <= mul_func_s3;
-            word_op_s4      <= word_op_s3;
+                        state           <= S_RUN_1;
+                    end
+                end
+                S_RUN_1: begin
+                    if (flush_i) begin
+                        state               <= S_IDLE;
+                    end else begin
+                        p0_q                <= p0_in;
+                        p1_q                <= p1_in;
+
+                        state               <= S_RUN_2;
+                    end
+                end
+                S_RUN_2: begin
+                    if (flush_i) begin
+                        state           <= S_IDLE;
+                    end else if (word_op) begin
+                        if (mul_ready_i) begin
+                            state       <= S_IDLE;
+                        end
+                    end else begin
+                        p2_q            <= p2_in;
+                        p3_q            <= p3_in;
+
+                        state           <= S_RUN_3;
+                    end
+                end 
+                S_RUN_3: begin
+                    if (flush_i) begin
+                        state               <= S_IDLE;
+                    end else begin
+                        part_sum_q          <= part_sum_in;
+
+                        state               <= S_RUN_4;
+                    end
+                end 
+                S_RUN_4: begin
+                    if (flush_i | mul_ready_i) begin
+                        state           <= S_IDLE;
+                    end 
+                end
+            endcase
         end
     end
 
-    // stage 5 - output
-    logic [127:0]   final_sum;
-    logic [63:0]    mul_res;
-    logic           mul_busy;
-
-    assign mul_busy             =   v_s1 | v_s2 | v_s3 | v_s4;
-
-    assign final_sum[127:0]     =   ({128{ negate_res_s4}} & twos_comp_128(full_sum_s4[127:0])) | 
-                                    ({128{~negate_res_s4}} & full_sum_s4[127:0]);
-    
     always_comb begin
-        case(mul_func_s4)
-            OP_MUL      :   mul_res =   ({64{ word_op_s4}} & {{32{final_sum[31]}}, final_sum[31:0]}) |
-                                        ({64{~word_op_s4}} & final_sum[63:0]);
-            OP_MULH,
-            OP_MULHU,
-            OP_MULHSU   :   mul_res =   final_sum[127:64];
-            default     :   mul_res =   64'h0;
+        mul_ready_o             =   1'b0;
+        mul_res_o               =   64'h0;
+        mul_res_valid_o         =   1'b0;
+
+        a_signed_in             =   1'b0;
+        b_signed_in             =   1'b0;
+        negate_res_in           =   1'b0;
+
+        a_negate_dw             =   1'b0;
+        a_correct_dw            =   1'b0;
+        a_negate_w              =   1'b0;
+        a_correct_w             =   1'b0;
+
+        b_negate_dw             =   1'b0;
+        b_correct_dw            =   1'b0;
+        b_negate_w              =   1'b0;
+        b_correct_w             =   1'b0;
+
+        a_mag                   =   64'h0;
+        b_mag                   =   64'h0;
+
+        a_high_in               =   32'h0;
+        a_low_in                =   32'h0;
+
+        b_high_in               =   32'h0;
+        b_low_in                =   32'h0;
+
+        p0_in                   =   64'h0;
+        p1_in                   =   64'h0;
+        p2_in                   =   64'h0;
+        p3_in                   =   64'h0;
+
+        cross_terms             =   65'h0;
+        part_sum_in             =   128'h0;
+
+        mul_res_corr            =   64'h0;
+        full_sum                =   128'h0;
+        final_sum               =   128'h0;
+
+        case (state)
+            S_IDLE: begin
+                a_signed_in     =   (mul_func_i == OP_MUL | mul_func_i == OP_MULH | mul_func_i == OP_MULHSU);
+                b_signed_in     =   (mul_func_i == OP_MUL | mul_func_i == OP_MULH);
+
+                negate_res_in   =   (~word_op_i & ((opr_a_i[63] & a_signed_in) ^ (opr_b_i[63] & b_signed_in))) | 
+                                    ( word_op_i &  (opr_a_i[31] ^ opr_b_i[31]));
+
+                a_negate_dw     =   a_signed_in & opr_a_i[63] & ~word_op_i;
+                a_correct_dw    =   (~a_signed_in | ~opr_a_i[63]) & ~word_op_i;
+                a_negate_w      =   word_op_i &  opr_a_i[31];
+                a_correct_w     =   word_op_i & ~opr_a_i[31];
+
+                a_mag           =   ({64{a_negate_dw}}  & (~opr_a_i + 64'b1))                   | 
+                                    ({64{a_correct_dw}} & ( opr_a_i))                           |
+                                    ({64{a_negate_w}}   & {32'h0, (~opr_a_i[31:0] + 32'b1)})    | 
+                                    ({64{a_correct_w}}  & {32'h0, opr_a_i[31:0]});
+
+                a_high_in       =   a_mag[63:32];
+                a_low_in        =   a_mag[31:0];
+
+                b_negate_dw     =   b_signed_in & opr_b_i[63] & ~word_op_i;
+                b_correct_dw    =   (~b_signed_in | ~opr_b_i[63]) & ~word_op_i;
+                b_negate_w      =   word_op_i &  opr_b_i[31];
+                b_correct_w     =   word_op_i & ~opr_b_i[31];
+
+                b_mag           =   ({64{b_negate_dw}}  & (~opr_b_i + 64'b1))                   | 
+                                    ({64{b_correct_dw}} & ( opr_b_i))                           | 
+                                    ({64{b_negate_w}}   & {32'h0, (~opr_b_i[31:0] + 32'b1)})    | 
+                                    ({64{b_correct_w}}  & {32'h0, opr_b_i[31:0]});
+
+                b_high_in       =   b_mag[63:32];
+                b_low_in        =   b_mag[31:0];
+
+                mul_ready_o     =   1'b1;
+            end
+            S_RUN_1: begin
+                p0_in           =   a_low_q * b_low_q;
+                p1_in           =   a_low_q * b_high_q;
+            end
+            S_RUN_2: begin
+                p2_in           =   a_high_q * b_low_q;
+                p3_in           =   a_high_q * b_high_q;
+
+                if (word_op) begin
+                    mul_res_corr            =   negate_res ? ~p0_q + 64'b1 : p0_q;
+                    mul_res_o               =   {{32{mul_res_corr[31]}}, mul_res_corr[31:0]};
+                    mul_res_valid_o         =   ~flush_i;
+                end
+            end
+            S_RUN_3: begin
+                cross_terms     =   {1'b0, p1_q} + {1'b0, p2_q};
+                part_sum_in     =   {64'h0, p0_q} + {31'h0, cross_terms, 32'h0};
+            end
+            S_RUN_4: begin
+                full_sum        =   part_sum_q + {p3_q, 64'h0};
+                final_sum       =   negate_res ? ~full_sum + 128'b1 : full_sum;
+
+                case (mul_func)
+                    OP_MUL:     mul_res_o   =   final_sum[63:0];
+                    OP_MULH,
+                    OP_MULHU, 
+                    OP_MULHSU:  mul_res_o   =   final_sum[127:64];
+                endcase
+                
+                mul_res_valid_o =   ~flush_i;
+            end
         endcase
     end
 
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            mul_res_o       <= 64'h0;
-            valid_res_o     <= 1'b0;
-            mul_busy_o      <= 1'b0;
-        end else begin
-            mul_res_o       <= mul_res;
-            valid_res_o     <= v_s4;
-            mul_busy_o      <= mul_busy;
-        end
-    end
 endmodule

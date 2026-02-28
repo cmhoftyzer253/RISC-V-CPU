e326ee9fea4e0d091451f06a57f48eb5f1ed78e4
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-02-28 00:35:36 -0500

    update interface

--- rtl/mem/clint.sv
+++ rtl/mem/clint.sv
@@ -2,23 +2,25 @@ import cpu_modules::*;
 
 module clint (
     input logic                 clk,
-    input logic                 reset,
+    input logic                 resetn,
 
-    input logic [63:0]          addr_i,
-    input logic                 valid_i,
-    input mem_access_size_t     byte_en_i,
-    input logic                 wr_i,
-    input logic [63:0]          wr_data_i,
-    output logic                ready_o,
+    input logic [63:0]          req_addr_i,
+    input logic                 req_valid_i,
+    input mem_access_size_t     req_byte_en_i,
+    input logic                 req_wr_i,
+    input logic [63:0]          req_wr_data_i,
+    output logic                req_ready_o,
 
-    output logic [63:0]         data_o,
-    output logic                resp_valid_o,
+    output logic [63:0]         clint_data_o,
+    output logic                clint_resp_valid_o,
 
-    output logic                msi_irq_o,
-    output logic                mti_irq_o,
+    output logic                msip_irq_o,
+    output logic                mtip_irq_o,
 
     output logic                exc_valid_o,
-    output logic [4:0]          exc_code_o
+    output logic [4:0]          exc_code_o,
+
+    output logic [63:0]         mtime_o
 );
 
     logic           msip_q;
@@ -34,7 +36,7 @@ module clint (
     logic           acc_fault;
 
     always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
+        if (~resetn) begin
             msip_q <= 1'b0;
             mtimecmp_q <= 64'hFFFF_FFFF_FFFF_FFFF;
             mtime_q <= 64'h0;
@@ -53,45 +55,46 @@ module clint (
     end
 
     always_comb begin
-        data_o                  =   64'h0;
-        resp_valid_o            =   1'b0;
+        clint_data_o            =   64'h0;
+        clint_resp_valid_o      =   1'b0;
         
-        ready_o                 =   1'b1;
+        req_ready_o             =   1'b1;
 
-        msip_addr               =   (addr_i == 64'h0000_0000_0001_0000);
-        mtimecmp_addr           =   (addr_i == 64'h0000_0000_0001_4000);
-        mtime_addr              =   (addr_i == 64'h0000_0000_0001_BFF8);
+        msip_addr               =   (req_addr_i == 64'h0000_0000_0200_0000);
+        mtimecmp_addr           =   (req_addr_i == 64'h0000_0000_0200_4000);
+        mtime_addr              =   (req_addr_i == 64'h0000_0000_0200_BFF8);
 
         oob                     =   ~(msip_addr | mtimecmp_addr | mtime_addr);
 
         acc_fault               =   (mtimecmp_addr | mtime_addr) & 
                                     ((byte_en_i == BYTE | byte_en_i == HALF_WORD | byte_en_i == WORD) | (|addr_i[2:0]));
 
-        handshake               =   valid_i & ready_o;
+        req_handshake           =   req_valid_i & req_ready_o;
 
-        exc_valid_o             =   (oob | acc_fault) & handshake;
-        exc_code_o              =   wr_i ? 5'd7 : 5'd5;
+        exc_valid_o             =   (oob | acc_fault) & req_handshake;
+        exc_code_o              =   req_wr_i ? 5'd7 : 5'd5;
 
-        msi_irq_o               =   msip_q;
-        mti_irq_o               =   mtime_q >= mtimecmp_q;
+        msip_irq_o              =   msip_q;
+        mtip_irq_o              =   mtime_q >= mtimecmp_q;
+
+        mtime_o                 =   mtime_q;
 
         //loads
         if (~wr_i & handshake & ~exc_valid_o) begin
             if (msip_addr) begin
-                data_o          =   {63'h0, msip_q};
-                resp_valid_o    =   1'b1;
+                clint_data_o        =   {63'h0, msip_q};
+                clint_resp_valid_o  =   1'b1;
             end else if (mtimecmp_addr) begin
-                data_o          =   mtimecmp_q;
-                resp_valid_o    =   1'b1;
+                clint_data_o        =   mtimecmp_q;
+                clint_resp_valid_o  =   1'b1;
             end else if (mtime_addr) begin
-                data_o          =   mtime_q;
-                resp_valid_o    =   1'b1;
+                clint_data_o        =   mtime_q;
+                clint_resp_valid_o  =   1'b1;
             end 
         end else begin
-            data_o              =   64'h0;
-            resp_valid_o        =   handshake & ~exc_valid_o;
+            clint_data_o            =   64'h0;
+            clint_resp_valid_o      =   handshake & ~exc_valid_o;
         end
-
     end
 
 endmodule

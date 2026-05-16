70da54ed9666a792fedc50b0c9008e569cdec7cc
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-05-16 05:17:01 -0400

    update golden model for funct3/funct7 error detection

--- tb/decode/decode_golden.c
+++ tb/decode/decode_golden.c
@@ -1,17 +1,36 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 void decode_golden (
     uint32_t instr_i, 
-    uint32_t *rs1_o, uint32_t *rs2_o, uint8_t *rd_o, uint32_t *op_o, uint32_t *funct3_o, uint32_t *funct12_o, uint32_t *csr_addr_o, 
+    uint32_t *rs1_o, uint32_t *rs2_o, uint32_t *rd_o, uint32_t *op_o, uint32_t *funct3_o, uint32_t *funct12_o, uint32_t *csr_addr_o, 
     uint32_t *r_type_o, uint32_t *i_type_o, uint32_t *s_type_o, uint32_t *b_type_o, uint32_t *u_type_o, uint32_t *j_type_o, uint32_t *system_type_o,
     uint64_t *imm_o, uint32_t *exc_valid_o, uint32_t *exc_code_o
 ) {
+    uint32_t exc_opcode = 0;
+    uint32_t exc_funct;
 
-    uint32_t r_type = ((instr_i & 0x7F) == 0x33) | ((instr_i & 0x7F) == 0x3B);
-    uint32_t i_type = ((instr_i & 0x7F) == 0x03) | ((instr_i & 0x7F) == 0x13) | ((instr_i & 0x7F) == 0x67) | ((instr_i & 0x7F) == 0x1B);
+    uint32_t exc_funct_rtype_0;
+    uint32_t exc_funct_rtype_1;
+    uint32_t exc_funct_rtype;
+
+    uint32_t r_type_0 = ((instr_i & 0x7F) == 0x33);
+    uint32_t r_type_1 = ((instr_i & 0x7F) == 0x3B);
+    uint32_t r_type = r_type_0 | r_type_1;
+
+    uint32_t i_type_0 = ((instr_i & 0x7F) == 0x03);
+    uint32_t i_type_1 = ((instr_i & 0x7F) == 0x13);
+    uint32_t i_type_2 = ((instr_i & 0x7F) == 0x67);
+    uint32_t i_type_3 = ((instr_i & 0x7F) == 0x1B);
+    uint32_t i_type = i_type_0 | i_type_1 | i_type_2 | i_type_3;
+
     uint32_t s_type = ((instr_i & 0x7F) == 0x23);
     uint32_t b_type = ((instr_i & 0x7F) == 0x63);
-    uint32_t u_type = ((instr_i & 0x7F) == 0x37) | ((instr_i & 0x7F) == 0x17);
+
+    uint32_t u_type_0 = ((instr_i & 0x7F) == 0x37);
+    uint32_t u_type_1 = ((instr_i & 0x7F) == 0x17);
+    uint32_t u_type = u_type_0 | u_type_1;
+
     uint32_t j_type = ((instr_i & 0x7F) == 0x6F);
     uint32_t system_type = ((instr_i & 0x7F) == 0x73);
 
@@ -76,7 +95,100 @@ void decode_golden (
         *csr_addr_o = (instr_i & 0xFFF00000) >> 20;
         *imm_o = (instr_i & 0xF8000) >> 15;
     } else {
-        *exc_valid_o = 1;
-        *exc_code_o = 2;
+        exc_opcode = 1;
     }
+
+    uint32_t funct7 = (*funct12_o >> 5) & 0x7F;
+    uint32_t funct6 = (funct7 >> 1) & 0x3F;
+
+    uint32_t funct3 = *funct3_o;
+    uint32_t funct12 = *funct12_o;
+    uint32_t opcode = (instr_i & 0x7F);
+
+    bool legal_rtype_0 = false;
+    bool legal_rtype_1 = false;
+    bool legal_itype_0 = false;
+    bool legal_itype_1 = false;
+    bool legal_itype_2 = false;
+    bool legal_itype_3 = false;
+    bool legal_stype = false;
+    bool legal_btype = false;
+    bool legal_systemtype = false;
+
+    switch (opcode) {
+        case 0x33: 
+            switch (funct7) {
+                case 0x00: legal_rtype_0 = true; break;
+                case 0x01: legal_rtype_0 = true; break;
+                case 0x20: legal_rtype_0 = (*funct3_o == 0b000) || (*funct3_o == 0b101); break;
+                default: legal_rtype_0 = false;
+            }
+            break;
+
+        case 0x3B:
+            switch (funct7) {
+                case 0x00: 
+                    legal_rtype_1 = (funct3 == 0b000) || (funct3 == 0b001) || (funct3 == 0b101); 
+                    break;
+                case 0x01: 
+                    legal_rtype_1 = (funct3 == 0b000) || (funct3 == 0b100) || (funct3 == 0b101) || (funct3 == 0b110) || (funct3 == 0b111); 
+                    break;
+                case 0x20: 
+                    legal_rtype_1 = (funct3 == 0b000) || (funct3 == 0b101);
+                    break;
+                default: legal_rtype_1 = false;
+            }
+            break;
+        
+        case 0x03: 
+            legal_itype_0 = (funct3 != 0b111); break;
+
+        case 0x13:
+            switch (funct3) {
+                case 0b000: 
+                case 0b010: 
+                case 0b011:
+                case 0b100: 
+                case 0b110: 
+                case 0b111: legal_itype_1 = true; break;
+                case 0b001: legal_itype_1 = (funct6 == 0x0); break;                     
+                case 0b101: legal_itype_1 = (funct6 == 0x0) || (funct6 == 0x10); break;
+            }
+            break;
+
+        case 0x67:
+            legal_itype_2 = (funct3 == 0b000); break;
+
+        case 0x1B:
+            switch (funct3) {
+                case 0b000: legal_itype_3 = true; break;
+                case 0b001: legal_itype_3 = (funct7 == 0x0); break;
+                case 0b101: legal_itype_3 = (funct7 == 0x0) || (funct7 == 0x20); break;
+                default: legal_itype_3 = false;
+            }
+            break;
+
+        case 0x23:
+            legal_stype = (funct3 <= 0b011); break;
+
+        case 0x63:
+            legal_btype = (funct3 != 0b010) && (funct3 != 0b011); break;
+            
+        case 0x73:
+            switch (funct3) {
+                case 0b000:
+                    legal_systemtype = (funct12 == 0x000) || (funct12 == 0x001) || (funct12 == 0x302) || (funct12 == 0x105); break;
+                case 0b100:
+                    legal_systemtype = false; break;
+                default:
+                    legal_systemtype = true; break;
+            }
+            break;
+    }
+
+    exc_funct = !(legal_rtype_0 || legal_rtype_1 || legal_itype_0 || legal_itype_1 || legal_itype_2 || legal_itype_3 
+                || legal_stype || legal_btype || legal_systemtype || u_type || j_type);
+
+    *exc_valid_o = exc_opcode || exc_funct;
+    *exc_code_o = *exc_valid_o ? 2 : 0;
 }

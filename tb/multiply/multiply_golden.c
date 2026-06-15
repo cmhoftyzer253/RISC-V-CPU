004dac79e9cef699176a37f37d2345fdfb9be54a
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-06-15 14:56:15 -0400

    golden model fix

--- tb/multiply/multiply_golden.c
+++ tb/multiply/multiply_golden.c
@@ -2,10 +2,8 @@
 #include <multiply_golden.h>
 
 void multiply_golden (
-    uint32_t resetn,
-    uint64_t opr_a_i, uint64_t opr_b_i, uint32_t mul_valid_i, uint32_t mul_func_i, uint32_t word_op_i, uint32_t *mul_ready_o,
-    uint32_t flush_i,
-    uint32_t mul_res_ready_i, uint64_t *mul_res_o, uint32_t *mul_res_valid_o
+    uint64_t opr_a_i, uint64_t opr_b_i, uint32_t mul_func_i, uint32_t word_op_i,
+    uint64_t *mul_res_o
 ) {
     int128_t mul_res128;
     uint128_t mul_ures128;
@@ -18,9 +16,6 @@ void multiply_golden (
     int32_t opr_b32 = (int32_t)opr_b_i;
     uint32_t opr_ua32 = (uint32_t)opr_a_i;
     uint32_t opr_ub32 = (uint32_t)opr_b_i;
-
-    *mul_ready_o = !mul_res_ready_i;
-    *mul_res_valid_o = !flush_i && mul_valid_i;
 
     switch (mul_func_i) {
         case OP_MUL:
@@ -35,7 +30,7 @@ void multiply_golden (
             *mul_res_o = (uint64_t)(mul_res128 >> 64);
             break;
         case OP_MULHSU:
-            mul_res128 = (int128_t)opr_a64 * (int128_t)opr_b64;
+            mul_res128 = (int128_t)opr_a64 * (int128_t)(uint128_t)opr_b_i;
 
             *mul_res_o = (uint64_t)(mul_res128 >> 64);
             break;
@@ -43,75 +38,6 @@ void multiply_golden (
             mul_ures128 = (uint128_t)opr_a_i * (uint128_t)opr_b_i;
 
             *mul_res_o = (uint64_t)(mul_ures128 >> 64);
-            break;
-        case OP_DIV:
-            if (word_op_i) {
-                if (opr_b32 == 0){
-                    *mul_res_o = 0xFFFFFFFFFFFFFFFF;
-                } else if (opr_a32 == (int32_t)0x80000000 && opr_b32 == -1) {
-                    *mul_res_o = (uint64_t)(int64_t)opr_a32;
-                } else {
-                    *mul_res_o = (uint64_t)(int64_t)(opr_a32 / opr_b32);
-                }
-            } else {
-                if (opr_b64 == 0) {
-                    *mul_res_o = 0xFFFFFFFFFFFFFFFF;
-                } else if (opr_a64 == 0x8000000000000000 && opr_b64 == -1) {
-                    *mul_res_o = (uint64_t)opr_a64;
-                } else {
-                    *mul_res_o = (uint64_t)(opr_a64 / opr_b64);
-                }
-            }
-            break;
-        case OP_DIVU:
-            if (word_op_i) {
-                if (opr_ub32 == 0) {
-                    *mul_res_o = 0xFFFFFFFFFFFFFFFF;
-                } else {
-                    mul_ures32 = opr_ua32 / opr_ub32;
-                    *mul_res_o = (uint64_t)(int32_t)mul_ures32;
-                }
-            } else {
-                if (opr_b_i == 0) {
-                    *mul_res_o = 0xFFFFFFFFFFFFFFFF;
-                } else {
-                    *mul_res_o = opr_a_i / opr_b_i;
-                }
-            }
-            break;
-        case OP_REM:
-            if (word_op_i) {
-                if (opr_b32 == 0) {
-                    *mul_res_o = (uint64_t)(int64_t)opr_a32;
-                } else if (opr_a32 == (int32_t)0x80000000 && opr_b32 == -1) {
-                    *mul_res_o = 0;
-                } else {
-                    *mul_res_o = (uint64_t)(int64_t)(opr_a32 % opr_b32);
-                }
-            } else {
-                if (opr_b64 == 0) {
-                    *mul_res_o = (uint64_t)opr_a64;
-                } else if (opr_a64 == (int64_t)0x8000000000000000 && opr_b64 == -1) {
-                    *mul_res_o = 0;
-                } else {
-                    *mul_res_o = (uint64_t)(opr_a64 % opr_b64);
-                }
-            }
-            break;
-        case OP_REMU:
-            if (word_op_i) {
-                if (opr_ub32 == 0) {
-                    *mul_res_o = (uint64_t)(int64_t)(int32_t)opr_ua32;
-                } else {
-                    *mul_res_o = (uint64_t)(int64_t)(int32_t)(opr_ua32 % opr_ub32);
-                }
-            } else {
-                if (opr_b_i == 0) {
-                    *mul_res_o = opr_a_i;
-                } else {
-                    *mul_res_o = opr_a_i % opr_b_i;
-                }
-            }
             break;
     }
 
--- tb/multiply/multiply_golden.h
+++ tb/multiply/multiply_golden.h
@@ -5,7 +5,3 @@ typedef unsigned __int128 uint128_t;
 #define OP_MULH 0x01
 #define OP_MULHSU 0x02
 #define OP_MULHU 0x03
-#define OP_DIV 0x04
-#define OP_DIVU 0x05
-#define OP_REM 0x06
-#define OP_REMU 0x07

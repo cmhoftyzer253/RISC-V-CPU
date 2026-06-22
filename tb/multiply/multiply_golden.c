481a637d0153406acd14e49350de25fa10df1be0
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-06-22 03:42:54 -0400

    update golden model header types

--- tb/multiply/multiply_golden.c
+++ tb/multiply/multiply_golden.c
@@ -18,23 +18,23 @@ void multiply_golden (
     uint32_t opr_ub32 = (uint32_t)opr_b_i;
 
     switch (mul_func_i) {
-        case OP_MUL:
+        case MUL:
             mul_ures64 = opr_a_i * opr_b_i;
             mul_ures32 = opr_ua32 * opr_ub32;
 
             *mul_res_o = word_op_i ? (uint64_t)(int64_t)(int32_t)mul_ures32 : mul_ures64;
             break;
-        case OP_MULH:
+        case MULH:
             mul_res128 = (int128_t)opr_a64 * (int128_t)opr_b64;
 
             *mul_res_o = (uint64_t)(mul_res128 >> 64);
             break;
-        case OP_MULHSU:
+        case MULHSU:
             mul_res128 = (int128_t)opr_a64 * (int128_t)(uint128_t)opr_b_i;
 
             *mul_res_o = (uint64_t)(mul_res128 >> 64);
             break;
-        case OP_MULHU:
+        case MULHU:
             mul_ures128 = (uint128_t)opr_a_i * (uint128_t)opr_b_i;
 
             *mul_res_o = (uint64_t)(mul_ures128 >> 64);
--- tb/multiply/multiply_golden.h
+++ tb/multiply/multiply_golden.h
@@ -1,7 +1,7 @@
 typedef __int128 int128_t;
 typedef unsigned __int128 uint128_t;
 
-#define OP_MUL 0x00
-#define OP_MULH 0x01
-#define OP_MULHSU 0x02
-#define OP_MULHU 0x03
+#define MUL 0x00
+#define MULH 0x01
+#define MULHSU 0x02
+#define MULHU 0x03

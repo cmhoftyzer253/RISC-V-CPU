f44831339dedb4439991197c659f91ef424f34d8
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-07-02 02:51:22 -0400

    push missing files

--- rtl/decode/decode.sv
+++ rtl/decode/decode.sv
@@ -1,25 +1,25 @@
 import cpu_consts::*;
 
 module decode (
-    input logic [31:0]  instr_i,
-    output logic [4:0]  rs1_o,
-    output logic [4:0]  rs2_o,
-    output logic [4:0]  rd_o,
-    output logic [6:0]  op_o,
-    output logic [2:0]  funct3_o,
-    output logic [11:0]  funct12_o,
-    output logic [11:0] csr_addr_o,
-    output logic        r_type_o,
-    output logic        i_type_o,
-    output logic        s_type_o,
-    output logic        b_type_o,
-    output logic        u_type_o,
-    output logic        j_type_o,
-    output logic        system_type_o,
-    output logic [63:0] imm_o,
+    input logic [31:0]      instr_i,
+    output logic [4:0]      rs1_o,
+    output logic [4:0]      rs2_o,
+    output logic [4:0]      rd_o,
+    output logic [6:0]      op_o,
+    output logic [2:0]      funct3_o,
+    output logic [11:0]     funct12_o,
+    output logic [11:0]     csr_addr_o,
+    output logic            r_type_o,
+    output logic            i_type_o,
+    output logic            s_type_o,
+    output logic            b_type_o,
+    output logic            u_type_o,
+    output logic            j_type_o,
+    output logic            system_type_o,
+    output logic [63:0]     imm_o,
 
-    output logic        exc_valid_o,
-    output exc_cause_t  exc_code_o
+    output logic            exc_valid_o,
+    output exc_cause_t      exc_code_o
 );
 
     exc_cause_t         exc_opcode;

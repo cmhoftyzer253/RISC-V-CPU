3674ffdab8841088daf41f5de8db165e47778e6f
cmhoftyzer253 <cmhoftyzer@gmail.com>
2026-08-31 00:35:50 -0400

    update alu types

--- rtl/execute/alu.sv
+++ rtl/execute/alu.sv
@@ -1,20 +1,20 @@
 import cpu_consts::*;
 
 module alu (
-        input logic [63:0]  opr_a_i,
-        input logic [63:0]  opr_b_i,
+        input logic [63:0]      opr_a_i,
+        input logic [63:0]      opr_b_i,
 
-        input logic         alu_valid_i,
-        alu_op_t            alu_func_i,
-        input logic         word_op_i,
+        input logic             alu_valid_i,
+        input logic [3:0]       alu_func_i,
+        input logic             word_op_i,
 
-        input logic         flush_i,
+        input logic             flushE_i,
 
-        output logic        valid_res_o,
-        output logic [63:0] alu_res_o
+        output logic [63:0]     alu_res_o
+        output logic            valid_res_o,
     );
 
-    logic [63:0] alu_res;
+    logic [63:0]                alu_res;
 
     always_comb begin                                                                   
         case (alu_func_i)
@@ -33,9 +33,7 @@ module alu (
         endcase
     end
 
-    assign alu_res_o    =   ({64{ word_op_i}} & {{32{alu_res[31]}}, alu_res[31:0]}) |
-                            ({64{~word_op_i}} & alu_res);
-
-    assign valid_res_o  =   alu_valid_i & ~flush_i;
+    assign alu_res_o    =   word_op_i ? {{32{alu_res[31]}}, alu_res[31:0]} : alu_res;
+    assign valid_res_o  =   alu_valid_i && !flushE_i;
 
 endmodule
